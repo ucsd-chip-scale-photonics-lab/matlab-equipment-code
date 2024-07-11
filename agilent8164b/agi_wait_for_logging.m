e1f8d2554ebe954642f99faeeebf20e3183c8557
Karl Johnson <k@rljohnson.net>
2024-07-11 11:41:16 -0700

    Added VISA timeout increase to logging funct

--- agilent8164b/agi_wait_for_logging.m
+++ agilent8164b/agi_wait_for_logging.m
@@ -1,16 +1,28 @@
-function didFinish = agi_wait_for_logging(agi, maxWaitTime)
+function didFinish = agi_wait_for_logging(agi, estLoggingTime)
     % poke Agilent until sweep is finished, return true if it finished in
-    % specified time
-    % if sweep is complete, we get this:
+    % expected amount of time
     if ~exist('maxWaitTime', 'var')
-        maxWaitTime = 30;
+        estLoggingTime = 15;
     end
+
+    % once the logging starts, the VISA communication line will become
+    % unresponsive until it finishes. So we should increase the VISA
+    % timeout to a little more than the estimated duration of logging
+    agi.Timeout = estLoggingTime + 3; 
+    % if the logging starts, it is almost guaranteed that it will
+    % eventually finish (in the absense of loss of connection etc.)
+
+    % if sweep is complete, we get this:
     completeString = ['LOGGING_STABILITY,COMPLETE' newline()]; 
     % if sweep is in progress, we get this:
     progressString = ['LOGGING_STABILITY,PROGRESS' newline()];
-    % no need to be fancy, just check and pause every second
+
+    % Check once per second, up to a maximum number of seconds - we use
+    % estLoggingTime for this for convenience, though once the sweep
+    % actually starts, the query will hang until it finishes. The only time
+    % we'll reach the full number of loops is if logging never starts.
     didFinish = false;
-    for waitIdx = 1:maxWaitTime
+    for waitIdx = 1:estLoggingTime
         thisResponse = query(agi, ":SENS2:CHAN1:FUNC:STAT?");
         fwrite(agi, '*WAI');
         if(strcmp(thisResponse, completeString))
